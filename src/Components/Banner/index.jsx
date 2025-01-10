853c00c38a8b3260afa6757eaa82eb8cc0d6aa9d
Luis Eduardo Dizu Mendez <dizu.eduardo@gmail.com>
2025-01-10 17:54:12 -0500

    Banner principal finalizado

--- src/Components/Banner/index.jsx
+++ src/Components/Banner/index.jsx
@@ -1,9 +1,30 @@
-import styles from "./Banner.module.css"
+import styles from "./Banner.module.css";
 
 const Banner = () => {
-    return(
-        <>
-        </>
-    )
-}
+  return (
+    <div className={styles.container}>
+
+        <div className={styles.inner_container}>
+
+            <div className={styles.inner_container_left}>
+
+                <span className={styles.afterTitle}>FRONT END</span>
+
+                <h1>Challenge React</h1>
+
+                <p>
+                    Este challenge es una forma de aprendizaje. Es un mecanismo donde
+                    podrás comprometerte en la resolución de un problema para poder
+                    aplicar todos los conocimientos adquiridos en la formación React.
+                </p>
+
+            </div>
+
+            <img className={styles.imagen} src="/src/assets/player.png" alt="Imagen principal" />
+
+        </div>
+
+    </div>
+  );
+};
 export default Banner;
--- src/Components/Footer/index.jsx
+++ src/Components/Footer/index.jsx
@@ -2,8 +2,9 @@ import styles from "./Footer.module.css"
 
 const Footer = () => {
     return(
-        <>
-        </>
+        <div className={styles.container}>
+            <img src="/src/assets/logo.png" alt="Logo Alura" />
+        </div>
     )   
 }
 export default Footer;
