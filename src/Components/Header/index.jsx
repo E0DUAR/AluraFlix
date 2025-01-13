7fa364721353d4e33d96c4e7ca2b1beccb296bf4
Eduardo Mendez <67878896+E0DUAR@users.noreply.github.com>
2025-01-12 20:42:48 -0500

    logo

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -16,7 +16,7 @@ const Header = ({ page }) => {
     <>
       <div className={container}>
       <Link to="/" element={<Inicio />}>
-        <img src="assets/logo.png" alt="Logo Alura" />
+        <img src="../assets/logo.png" alt="Logo Alura" />
         </Link>
 
         <div className={styles.menu}>
